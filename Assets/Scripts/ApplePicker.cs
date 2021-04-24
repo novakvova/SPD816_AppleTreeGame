6ea3473c63cb5bffe8db2229942182d9d853612c
novakvova <novakvova@gmail.com>
2021-04-24 17:54:59 +0300

    Is good

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header ("Set in Inspecktor")]
+    public GameObject basketPrefab;
+    public int numBasket = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       for (int i=0;i<numBasket;i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Получить текущие координаты указателя мыши на экране из Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // Координата Z камеры определяет, как далеко в трехмерном пространстве
+        // находится указатель мыши
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Преобразовать точку на двумерной плоскости экрана в трехмерные
+        // координаты игры
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Переместить корзину вдоль оси X в координату X указателя мыши
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+        }
     }
 }
